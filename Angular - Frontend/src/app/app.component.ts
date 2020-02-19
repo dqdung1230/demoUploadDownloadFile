8d33fbdf9c4fc3158646824d6922029c1b81ed70
dqdung1230 <dqdung96@gmail.com>
2020-02-19 17:15:53 +0700

    change api

--- Angular - Frontend/src/app/app.component.ts
+++ Angular - Frontend/src/app/app.component.ts
@@ -43,31 +43,34 @@ export class AppComponent implements OnInit {
 
 
   download() {
+    debugger;
     this.resetForm();
     const filename = sessionStorage.getItem('filename');
     const checkFileType = filename.split('.').pop();
     let fileType;
-    if (checkFileType === '.txt') {
+    if (checkFileType === 'txt') {
       fileType = 'text/plain';
-    } else if (checkFileType === '.pdf') {
+    } else if (checkFileType === 'pdf') {
       fileType = 'application/pdf';
-    } else if (checkFileType === '.doc' || checkFileType === '.docx') {
+    } else if (checkFileType === 'doc' || checkFileType === 'docx') {
       fileType = 'application/vnd.ms-word';
-    } else if (checkFileType === '.xls') {
+    } else if (checkFileType === 'xls') {
       fileType = 'application/vnd.ms-excel';
-    } else if (checkFileType === '.png') {
+    } else if (checkFileType === 'png') {
       fileType = 'image/png';
-    } else if (checkFileType === '.jpg' || checkFileType === '.jpeg') {
+    } else if (checkFileType === 'jpg' || checkFileType === 'jpeg') {
       fileType = 'image/jpeg';
-    } else if (checkFileType === '.gif') {
+    } else if (checkFileType === 'gif') {
       fileType = 'image/gif';
-    } else if (checkFileType === '.csv') {
+    } else if (checkFileType === 'csv') {
       fileType = 'text/csv';
-    } else if (checkFileType === '.mp3') {
+    } else if (checkFileType === 'mp3') {
       fileType = 'audio/mp3';
     }
+
     this.downloadService.downloadFile(filename, fileType).subscribe(success => {
         saveAs(success, filename);
+        alert('Download successful');
       },
       error => {
         alert('server error when downloading file');
